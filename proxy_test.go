505601134f7dcf29ec12dfdfee3008aa3d340d0d
Nathan Sullivan <nathan@nightsys.net>
2018-07-13 20:57:29 +1000

    work on test coverage

--- proxy_test.go
+++ proxy_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net"
+	"os"
 	"sync"
 	"testing"
 	"time"
@@ -26,21 +27,28 @@ func echoServer(c net.Conn) {
 	}
 }
 
-func DockerProxyMockTest(t *testing.T) {
+func TestDockerProxyMock(t *testing.T) {
 	// Start up a mocked Docker daemon unix socket, to receive calls on.
 	mocked_docker_daemon_socket_path := "/tmp/mock_docker.sock"
+	if _, err := os.Stat(mocked_docker_daemon_socket_path); err == nil {
+		os.Remove(mocked_docker_daemon_socket_path)
+	}
 	ml, err := net.Listen("unix", mocked_docker_daemon_socket_path)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 	// mocked_docker_daemon_socket, err
 	_, err = newStoppableUnixListener(ml)
+	defer os.Remove(mocked_docker_daemon_socket_path)
 	if err != nil {
 		t.Fatal(err.Error())
 	}
 	// TODO: make mocked_docker_daemon_socket do something useful
 
 	mocked_proxy_socket_path := "/tmp/mock_docker_proxy.sock"
+	if _, err := os.Stat(mocked_proxy_socket_path); err == nil {
+		os.Remove(mocked_proxy_socket_path)
+	}
 
 	// Start up the proxy
 	docker_proxy := dockerProxy{ListenSocket: mocked_proxy_socket_path, TargetSocket: mocked_docker_daemon_socket_path}
@@ -49,18 +57,21 @@ func DockerProxyMockTest(t *testing.T) {
 	wg.Add(1)
 	go docker_proxy.runProxy(&wg, ready)
 	<-ready
+	defer os.Remove(mocked_proxy_socket_path)
 
 	// Make a connection to the proxy, to fire off some commands
 	c, err := net.Dial("unix", mocked_proxy_socket_path)
 	if err != nil {
 		panic(err.Error())
 	}
+	println("proceeding...")
 	for {
+		println("sending...")
 		_, err := c.Write([]byte("hi\n"))
 		if err != nil {
 			println(err.Error())
 		}
-		time.Sleep(1e9)
+		time.Sleep(1 * time.Second)
 	}
 
 }
